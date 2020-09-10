eda855bbbd3b5377e99b6e66981578ec1aa9795a
shuwithu <weishu140@gmail.com>
2020-09-10 23:46:14 +0100

    grow the dragon

---
+++ TripleX/tripleX.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Let's try build a red dragon together";
+    std::cout << std::end1;
+    std::cout << "Enter the correct code to continue";
+    return 0;
+}

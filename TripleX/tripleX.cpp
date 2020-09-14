9d153f593430a570d05e3928af91dbfd9182c2ab
shuwithu <weishu140@gmail.com>
2020-09-14 23:56:49 +0100

    reset complier path

--- TripleX/tripleX.cpp
+++ TripleX/tripleX.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     std::cout << "Let's try build a red dragon together";
-    std::cout << std::end1;
+    std::cout << std::endl;
     std::cout << "Enter the correct code to continue";
     return 0;
 }
